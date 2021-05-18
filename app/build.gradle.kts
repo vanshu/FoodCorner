a1ae683177a04f15699b6dba1b4e9789936fa0c9
clarkshu <sxchao2008@gmail.com>
2021-05-18 21:31:42 +0900

    Add library into gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -26,6 +29,13 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        dataBinding =true
+        viewBinding = true
+
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -46,7 +56,69 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    testImplementation("junit:junit:4.+")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+
+
+    // Coordinator Layout
+    implementation ("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
+
+    // Material Components
+    implementation ("com.google.android.material:material:1.4.0-beta01")
+
+    // Navigation Component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
+
+    // Room components
+    implementation ("androidx.room:room-runtime:2.3.0")
+    kapt ("androidx.room:room-compiler:2.3.0")
+    implementation ("androidx.room:room-ktx:2.3.0")
+    androidTestImplementation ("androidx.room:room-testing:2.3.0")
+
+    // Data binding
+    kapt ("com.android.databinding:compiler:3.2.0-alpha10")
+    kapt ("androidx.databinding:databinding-common:4.2.1")
+
+    // DataStore
+    implementation ("androidx.datastore:datastore-preferences:1.0.0-beta01")
+
+    // Recyclerview
+    implementation ("androidx.recyclerview:recyclerview:1.2.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Dagger - Hilt
+    implementation ("com.google.dagger:hilt-android:2.33-beta")
+    kapt ("com.google.dagger:hilt-android-compiler:2.33-beta")
+
+    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+
+    // Lifecycle
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+
+    // Image Loading library Coil
+    implementation ("io.coil-kt:coil:1.1.1")
+
+    // Gson
+    implementation ("com.google.code.gson:gson:2.8.6")
+
+    // Shimmer
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
+    implementation ("com.todkars:shimmer-recyclerview:0.4.0")
+
+    // Jsoup
+    implementation ("org.jsoup:jsoup:1.13.1")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,9 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha15")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
-
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
     }
