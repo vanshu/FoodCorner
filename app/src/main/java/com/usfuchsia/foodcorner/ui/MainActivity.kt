9a1733ec58f16977a67b4e3f638a57ec57c4ff57
clarkshu <sxchao2008@gmail.com>
2021-05-19 17:25:08 +0900

    Update package class layout

--- app/src/main/java/com/usfuchsia/foodcorner/ui/MainActivity.kt
+++ app/src/main/java/com/usfuchsia/foodcorner/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.usfuchsia.foodcorner
+package com.usfuchsia.foodcorner.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.usfuchsia.foodcorner.R
 import com.usfuchsia.foodcorner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/usfuchsia/foodcorner/ui/fragment/FoodJokeFragment.kt
+++ app/src/main/java/com/usfuchsia/foodcorner/ui/fragment/FoodJokeFragment.kt
@@ -1,4 +1,4 @@
-package com.usfuchsia.foodcorner.ui
+package com.usfuchsia.foodcorner.ui.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
