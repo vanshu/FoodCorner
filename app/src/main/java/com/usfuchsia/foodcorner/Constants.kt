dbd0e23dffc5257e571d365c5c9fc272afc4dddf
clarkshu <sxchao2008@gmail.com>
2021-05-26 22:18:15 +0900

    Add FoodRecipeApi

---
+++ app/src/main/java/com/usfuchsia/foodcorner/Constants.kt
@@ -0,0 +1,8 @@
+package com.usfuchsia.foodcorner
+
+class Constants {
+    companion object{
+        const val BASE_URL=" https://api.spoonacular.com"
+        const val APK_KEY="333671bd97044c21a11b70d83b2b57ad"
+    }
+}
---
+++ app/src/main/java/com/usfuchsia/foodcorner/data/RemoteDataSource.kt
@@ -0,0 +1,14 @@
+package com.usfuchsia.foodcorner.data
+
+import com.usfuchsia.foodcorner.data.FoodRecipeApi
+import com.usfuchsia.foodcorner.model.FoodRecipes
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipeApi: FoodRecipeApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipes> {
+        return foodRecipeApi.getRecipes(queries)
+    }
+}
